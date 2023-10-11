c7092e83ed251187eac063a1f57cf6445d449301
Ndindairene <irene.ndindaa@gmail.com>
2023-10-11 14:03:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.getElementById('text');
+    text.innerHTML = 'This is really cool!';
+    console.log('JavaScript is so cool. It lets me add text to my page programmatically.');
+
+  });
